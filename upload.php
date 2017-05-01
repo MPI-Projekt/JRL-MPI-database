57aa9862ed945c910a64a542386fa08f1611c673
LarsMirra <LarsMirra@users.noreply.github.com>
2017-05-01 17:03:10 +0200

    mal sehen opb's klappt

--- upload.php
+++ upload.php
@@ -1,15 +1,16 @@
 <?php
 	
 	if (isset($_FILES['datei']['tmp_name'])) {
-		move_uploaded_file($_FILES['datei']['tmp_name'], 'H:\FZ'.$_FILES['datei']['name']);
-		}
-	
-	$file = $_FILES['datei']['name']
-	
-
+		move_uploaded_file($_FILES['datei']['tmp_name'], 'FZ/'.$_FILES['datei']['name']);
+	 }
+	$file = $_FILES['datei']['name'];
+	$path = 'FZ/'.$_FILES['datei']['name'];
+	$filename = 'name';
+	$filesize = filesize($path);
+	// Rest kommt von Jakob
 ?>
 <html>
 	<script>
-		window.open("einabe.php","_self")
+		window.open("eingabe.php","_self")
 	</script>
 </html>
