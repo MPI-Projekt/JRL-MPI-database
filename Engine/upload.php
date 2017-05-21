9b8b1f4f39c5ef26adfd459e6a08068f61de6cf9
LegendOfGoat <nrob498@gmail.com>
2017-05-21 14:41:20 +0200

    Update upload.php

--- Engine/upload.php
+++ Engine/upload.php
@@ -3,6 +3,7 @@
 	if (isset($_FILES['datei']['tmp_name'])) {
 		move_uploaded_file($_FILES['datei']['tmp_name'], 'FZ/'.$_FILES['datei']['name']);
 	 }
+	//$description = 
 	$file = $_FILES['datei']['name'];
 	$path = 'FZ/'.$_FILES['datei']['name'];
 	$filename = 'name';
