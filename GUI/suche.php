06a070b034798c3ff3bf339611f4a5876da03e4b
LarsMirra <LarsMirra@users.noreply.github.com>
2017-06-02 22:12:00 +0200

    GUI 31.05.2017

---
+++ GUI/suche.php
@@ -0,0 +1,42 @@
+<html>
+	<script>
+		window.open("ergebnis.html","_self")
+	</script>
+</html>
+
+
+
+<?php
+
+require_once ('konfiguration.php');
+    $db_link = mysqli_connect (
+                     MYSQL_HOST, 
+                     MYSQL_BENUTZER, 
+                     MYSQL_KENNWORT, 
+                     MYSQL_DATENBANK
+                    );
+				
+
+ if($dateiname != "" && $dateigroesse != ""){
+	 if($groesse == "groesser"){
+		$sql = "SELECT " .$reference. " FROM `files` WHERE `name` LIKE " .$dateiname.  " AND `size` > " .$dateigroesse. ";";
+	 }
+ }
+ 
+ 
+ $db_erg = mysqli_query($db_link, $sql);
+ 
+ echo '<table border="1">';
+while ($zeile = mysqli_fetch_array( $db_erg, MYSQL_ASSOC)){
+
+  echo "<tr>";
+  echo "<td>". $zeile['reference'] . "</td>";
+  echo "</tr>";
+}
+echo "</table>";
+?>
+<html>
+	<script>
+		window.open("ergebnis.html","_self")
+	</script>
+</html>
