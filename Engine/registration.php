100d0ddaf087c342fa2cbef5252affc25e89a269
LarsMirra <LarsMirra@users.noreply.github.com>
2017-06-10 01:19:15 +0200

    Engine 10.06.2017 01:19

--- Engine/registration.php
+++ Engine/registration.php
@@ -22,29 +22,19 @@ $pw1 = $_POST["pw1"];
 $pw2 = $_POST["pw2"];
 $benutzername = $_POST["benutzername"];	
 
-if($pw1 == $pw2){
-	<html>	
-		<script>
-			window.open("registrationfehlerhaft.html?error=passwort","_self")
-		</script>
-    </html>	
+if($pw1 != $pw2){
+	setcookie("error", 'passwort');
+	include("registrationfehlerhaft.html");
 }
 else{
 $erg1 = $db->query("SELECT name FROM user WHERE name LIKE ".$benutzername);
-if($erg1->num_rows){
-	<html>	
-		<script>
-			window.open("registrationfehlerhaft.html?error=benutzername","_self")
-		</script>
-	</html>	
-}
-else{
-$erg2 = $db->query("INSERT INTO user (name, password, database) VALUES ('" . $benutzername . "' , '" . $pw1 . "' , 'fs-" . $benutzername . "')");
-	<html>	
-		<script>
-			window.open("home.html","_self")
-		</script>
-	</html>	
+	if($erg1 != null){
+		setcookie("error", 'benutzername');
+		include("registrationfehlerhaft.html");
+	}
+	else{
+		$erg2 = $db->query("INSERT INTO user (name, password, database) VALUES ('" . $benutzername . "' , '" . $pw1 . "' , 'fs-" . $benutzername . "')");
+		include("home.html");
 	}
 }	
 ?>
