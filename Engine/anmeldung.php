27d43ac4f1732374d0416b014c9239cb64e9703f
SihdiST <sihdi.st@gmail.com>
2017-06-09 21:11:20 +0200

    Update Anmeldung

--- Engine/anmeldung.php
+++ Engine/anmeldung.php
@@ -7,15 +7,7 @@ $db = new mysqli(
 					 user
                      );
 
-if($db->connect_errno){
-require('user-datenbankErstellen.php');
-$db = new mysqli(
-                     MYSQL_HOST, 
-                     MYSQL_BENUTZER, 
-                     MYSQL_KENNWORT, 
-					 user
-                     );
-}	
+	
 $erg = $db->query("Select database WHERE name LIKE " .$benutzername. " AND password LIKE " .$passwort);
 if($erg->num_rows){
 $array = $erg->fetch_assoc();
