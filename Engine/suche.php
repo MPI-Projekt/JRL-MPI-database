79c31e0f4afddbb551c2c5e00484019aebe08591
SihdiST <sihdi.st@gmail.com>
2017-06-14 15:16:42 +0200

    Engine 14.06 15:16

--- Engine/suche.php
+++ Engine/suche.php
@@ -7,11 +7,92 @@ require('konfiguration.php');
                      MYSQL_DATENBANK
                     );
 				
+$dateiname = $_POST["dateiname"];
+$dateigroesse = $_POST["dateigroesse"];
+$groesse = $_POST["groesse"];
+$beschreibung = $_POST["beschreibung"];
+$typ = $_POST["dateityp"];
 
- if($dateiname != "" && $dateigroesse != ""){
+ if($dateiname != "" && $dateigroesse != "" && $beschreibung != "" && $typ != ""){
 	 if($groesse == "groesser"){
-		$erg = $db->query("SELECT " .$reference. " FROM `files` WHERE `name` LIKE " .$dateiname.  " AND `size` > " .$dateigroesse);
+		$erg = $db->query("SELECT reference FROM `files` WHERE `name` LIKE '" .$dateiname.  "' AND `size` > " .$dateigroesse. " AND `type` LIKE '" .$typ. "' AND `description` LIKE '" .$beschreibung. "'");
+	 }
+	 else if($groesse == "kleiner"){
+		$erg = $db->query("SELECT reference FROM `files` WHERE `name` LIKE '" .$dateiname.  "' AND `size` < " .$dateigroesse. " AND `type` LIKE '" .$typ. "' AND `description` LIKE '" .$beschreibung. "'"); 
+	 }
+	 else{
+		 $erg = $db->query("SELECT reference FROM `files` WHERE `name` LIKE '" .$dateiname.  "' AND `size` LIKE " .$dateigroesse. " AND `type` LIKE '" .$typ. "' AND `description` LIKE '" .$beschreibung. "'");
 	 }
  }
+ else if($dateiname != "" && $dateigroesse != "" && $beschreibung != ""){
+	  if($groesse == "groesser"){
+		$erg = $db->query("SELECT reference FROM `files` WHERE `name` LIKE '" .$dateiname.  "' AND `size` > " .$dateigroesse. " AND `description` LIKE '" .$beschreibung. "'");
+	 }
+	 else if($groesse == "kleiner"){
+		$erg = $db->query("SELECT reference FROM `files` WHERE `name` LIKE '" .$dateiname.  "' AND `size` < " .$dateigroesse. " AND `description` LIKE '" .$beschreibung. "'"); 
+	 }
+	 else{
+		 $erg = $db->query("SELECT reference FROM `files` WHERE `name` LIKE '" .$dateiname.  "' AND `size` LIKE " .$dateigroesse. " `description` LIKE '" .$beschreibung. "'");
+	 }
+ }
+ else if($dateiname != "" && $dateigroesse != ""){
+	  if($groesse == "groesser"){
+		$erg = $db->query("SELECT reference FROM `files` WHERE `name` LIKE '" .$dateiname.  "' AND `size` > " .$dateigroesse);
+	 }
+	 else if($groesse == "kleiner"){
+		$erg = $db->query("SELECT reference FROM `files` WHERE `name` LIKE '" .$dateiname.  "' AND `size` < " .$dateigroesse); 
+	 }
+	 else{
+		 $erg = $db->query("SELECT reference FROM `files` WHERE `name` LIKE '" .$dateiname.  "' AND `size` LIKE " .$dateigroesse);
+	 }
+ }
+ else if($dateiname != ""){
+	 $erg = $db->query("SELECT reference FROM `files` WHERE `name` LIKE '" .$dateiname.  "'");
+ }
+ else if($dateigroesse != "" && $beschreibung != "" && $typ != ""){
+	 if($groesse == "groesser"){
+		$erg = $db->query("SELECT reference FROM `files` WHERE `size` > " .$dateigroesse. " AND `type` LIKE '" .$typ. "' AND `description` LIKE '" .$beschreibung. "'");
+	 }
+	 else if($groesse == "kleiner"){
+		$erg = $db->query("SELECT reference FROM `files` WHERE `size` < " .$dateigroesse. " AND `type` LIKE '" .$typ. "' AND `description` LIKE '" .$beschreibung. "'"); 
+	 }
+	 else{
+		 $erg = $db->query("SELECT reference FROM `files` WHERE `size` LIKE " .$dateigroesse. " AND `type` LIKE '" .$typ. "' AND `description` LIKE '" .$beschreibung. "'");
+	 }
+ }
+ else if($dateigroesse != "" && $beschreibung != ""){
+	  if($groesse == "groesser"){
+		$erg = $db->query("SELECT reference FROM `files` WHERE `size` > " .$dateigroesse. " AND `description` LIKE '" .$beschreibung. "'");
+	 }
+	 else if($groesse == "kleiner"){
+		$erg = $db->query("SELECT reference FROM `files` WHERE `size` < " .$dateigroesse. " AND `description` LIKE '" .$beschreibung. "'"); 
+	 }
+	 else{
+		 $erg = $db->query("SELECT reference FROM `files` WHERE `size` LIKE " .$dateigroesse. " `description` LIKE '" .$beschreibung. "'");
+	 }
+ }
+ else if($dateigroesse != ""){
+	  if($groesse == "groesser"){
+		$erg = $db->query("SELECT reference FROM `files` WHERE `size` > " .$dateigroesse);
+	 }
+	 else if($groesse == "kleiner"){
+		$erg = $db->query("SELECT reference FROM `files` WHERE `name` LIKE '" .$dateiname.  "' AND `size` < " .$dateigroesse); 
+	 }
+	 else{
+		 $erg = $db->query("SELECT reference FROM `files` WHERE `name` LIKE '" .$dateiname.  "' AND `size` LIKE " .$dateigroesse);
+	 }
+ }
+ else if($beschreibung != "" && $typ != ""){	
+		 $erg = $db->query("SELECT reference FROM `files` WHERE `type` LIKE '" .$typ. "' AND `description` LIKE '" .$beschreibung. "'");
+	 
+ }
+ else if($beschreibung != ""){	 
+		$erg = $db->query("SELECT reference FROM `files` WHERE `description` LIKE '" .$beschreibung. "'");
+	
+ }
+ else if($typ != ""){
+		 $erg = $db->query("SELECT reference FROM `files` WHERE `type` LIKE " .$typ);
+	  }
+ 
 
 ?>
