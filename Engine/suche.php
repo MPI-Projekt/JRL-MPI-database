db1f861a0e9a767cd96888a2008dd5a9a48ed9b4
LarsMirra <LarsMirra@users.noreply.github.com>
2018-06-19 21:42:22 +0200

    Engine 19.06.2018 21:40
    Umstrukturierung der Datei. Nun nicht mehr viele einzelne SQL-Abfragen, sondern eine zusammengesetzte. 
    Ergänzung um Wahl zw. KB & MB, nicht wie bisher in Bytes.

--- Engine/suche.php
+++ Engine/suche.php
@@ -1,5 +1,6 @@
 <?php
-	session_start();
+session_start();
+error_reporting(0);
 require('konfiguration.php');
     $db = new mysqli(
                      MYSQL_HOST, 
@@ -8,97 +9,107 @@ require('konfiguration.php');
                      MYSQL_DATENBANK
                     );
 				
-$dateiname = $_POST["dateiname"];	
-$dateigroesse = $_POST["dateigroesse"];
-$groesse = $_POST["groesse"];
-$beschreibung = $_POST["beschreibung"];
-$typ = $_POST["dateityp"];
+$dateiname = $_SESSION["dateiname"];
+$dateigroesse = $_SESSION["dateigroesse"];
+$groesse = $_SESSION["groesse"];
+$beschreibung = $_SESSION["beschreibung"];
+$typ = $_SESSION["dateityp"];
+$byte = $_SESSION["byte"];
 
- if($dateiname != "" && $dateigroesse != "" && $beschreibung != "" && $typ != ""){
-	 if($groesse == "groesser"){
-		$erg = $db->query("SELECT * FROM `files` WHERE `name` LIKE '" .$dateiname.  "' AND `size` > " .$dateigroesse. " AND `type` LIKE '" .$typ. "' AND `description` LIKE '" .$beschreibung. "'");
-	}
-	 else if($groesse == "kleiner"){
-		$erg = $db->query("SELECT * FROM `files` WHERE `name` LIKE '" .$dateiname.  "' AND `size` < " .$dateigroesse. " AND `type` LIKE '" .$typ. "' AND `description` LIKE '" .$beschreibung. "'"); 
-	 }
-	 else{
-		 $erg = $db->query("SELECT * FROM `files` WHERE `name` LIKE '" .$dateiname.  "' AND `size` LIKE " .$dateigroesse. " AND `type` LIKE '" .$typ. "' AND `description` LIKE '" .$beschreibung. "'");
-	}
- }
- else if($dateiname != "" && $dateigroesse != "" && $beschreibung != ""){
-	  if($groesse == "groesser"){
-		$erg = $db->query("SELECT * FROM `files` WHERE `name` LIKE '" .$dateiname.  "' AND `size` > " .$dateigroesse. " AND `description` LIKE '" .$beschreibung. "'");
-	 }
-	 else if($groesse == "kleiner"){
-		$erg = $db->query("SELECT * FROM `files` WHERE `name` LIKE '" .$dateiname.  "' AND `size` < " .$dateigroesse. " AND `description` LIKE '" .$beschreibung. "'"); 
-	 }
-	 else{
-		 $erg = $db->query("SELECT * FROM `files` WHERE `name` LIKE '" .$dateiname.  "' AND `size` LIKE " .$dateigroesse. " `description` LIKE '" .$beschreibung. "'");
-	 }
- }
- else if($dateiname != "" && $dateigroesse != ""){
-	  if($groesse == "groesser"){
-		$erg = $db->query("SELECT * FROM `files` WHERE `name` LIKE '" .$dateiname.  "' AND `size` > " .$dateigroesse);
-	 }
-	 else if($groesse == "kleiner"){
-		$erg = $db->query("SELECT * FROM `files` WHERE `name` LIKE '" .$dateiname.  "' AND `size` < " .$dateigroesse); 
-	 }
-	 else{
-		 $erg = $db->query("SELECT * FROM `files` WHERE `name` LIKE '" .$dateiname.  "' AND `size` LIKE " .$dateigroesse);
-	 }
- }
- else if($dateiname != ""){
-	 $erg = $db->query("SELECT * FROM `files` WHERE `name` LIKE '" .$dateiname.  "'");
-	 }
- else if($dateigroesse != "" && $beschreibung != "" && $typ != ""){
-	 if($groesse == "groesser"){
-		$erg = $db->query("SELECT * FROM `files` WHERE `size` > " .$dateigroesse. " AND `type` LIKE '" .$typ. "' AND `description` LIKE '" .$beschreibung. "'");
-	 }
-	 else if($groesse == "kleiner"){
-		$erg = $db->query("SELECT * FROM `files` WHERE `size` < " .$dateigroesse. " AND `type` LIKE '" .$typ. "' AND `description` LIKE '" .$beschreibung. "'"); 
-	 }
-	 else{
-		 $erg = $db->query("SELECT * FROM `files` WHERE `size` LIKE " .$dateigroesse. " AND `type` LIKE '" .$typ. "' AND `description` LIKE '" .$beschreibung. "'");
-	 }
- }
- else if($dateigroesse != "" && $beschreibung != ""){
-	  if($groesse == "groesser"){
-		$erg = $db->query("SELECT * FROM `files` WHERE `size` > " .$dateigroesse. " AND `description` LIKE '" .$beschreibung. "'");
-	}
-	 else if($groesse == "kleiner"){
-		$erg = $db->query("SELECT * FROM `files` WHERE `size` < " .$dateigroesse. " AND `description` LIKE '" .$beschreibung. "'"); 
-	 }
-	 else{
-		 $erg = $db->query("SELECT * FROM `files` WHERE `size` LIKE " .$dateigroesse. " `description` LIKE '" .$beschreibung. "'");
-	 }
- }
- else if($dateigroesse != ""){
-	  if($groesse == "groesser"){
-		$erg = $db->query("SELECT * FROM `files` WHERE `size` > " .$dateigroesse);
-	 }
-	 else if($groesse == "kleiner"){
-		$erg = $db->query("SELECT * FROM `files` WHERE `name` LIKE '" .$dateiname.  "' AND `size` < " .$dateigroesse); 
-        $_SESSION['result'] = $erg;
-	}
-	 else{
-		 $erg = $db->query("SELECT * FROM `files` WHERE `name` LIKE '" .$dateiname.  "' AND `size` LIKE " .$dateigroesse); 
-	}
- }
- else if($beschreibung != "" && $typ != ""){	
-		 $erg = $db->query("SELECT * FROM `files` WHERE `type` LIKE '" .$typ. "' AND `description` LIKE '" .$beschreibung. "'");
- }
- else if($beschreibung != ""){	 
-		$erg = $db->query("SELECT * FROM `files` WHERE `description` LIKE '" .$beschreibung. "'");
- }
- else if($typ != ""){
-		 $erg = $db->query("SELECT * FROM `files` WHERE `type` LIKE " .$typ);
+if($groesse == "groesser"){
+	$groesse = ">";
+} else if($groesse == "kleiner"){
+	$groesse = "<";
+} else {
+	$groesse = "LIKE";
 }
-else{
-	$erg = $db->query("SELECT * FROM `files`");
-}
- 
-         $_SESSION['result'] = $erg;	
-		 header("location: ergebnis.php");
-		 //print_r($erg);
 
+if($byte == "KB"){
+	$dateigroesse = $dateigroesse * 1000;
+} else if ($byte == "MB") {
+	$dateigroesse = $dateigroesse * 1000000;
+}
+
+if($dateiname != ""){
+	$dateiname = " `name` LIKE '%%" .$dateiname.  "%%' ";
+} else {
+	$dateiname = "TRUE";
+}
+if($beschreibung != ""){
+	$beschreibung = " `description` LIKE '" .$beschreibung. "' ";
+} else {
+	$beschreibung = "TRUE";
+}
+if($typ != ""){
+	$typ = " `type` LIKE '" .$typ. "' ";
+} else {
+	$typ = "TRUE";
+}
+if($dateigroesse != ""){
+	$dateigroesse = " `size` " .$groesse.$dateigroesse. " ";
+} else {
+	$dateigroesse = "TRUE";
+}
+
+$erg = $db->query("SELECT * FROM `files` WHERE " .$dateiname. " AND " .$beschreibung. " AND " .$typ. " AND " .$dateigroesse);
+       
+echo '<html>';
+		echo '<meta charset="utf-8">';
+		echo '<head>';
+		echo '<link rel="icon" href="bilder/JRL-Database.ico">';
+		echo '</head>';
+		echo '<body bgcolor="#F5F5F5" text="#000000">';
+			echo '<u><font size="6"><p align="center">Ergebnis</p></font></u>';
+			echo '<div align="center">';	
+			echo '<br><br>';
+			if($erg->num_rows){ 
+					
+				    echo '<table style="border-collapse: collapse" border="1">';
+				    echo '<tr>';
+						echo '<th style="width:15%">Dateiname</th>';
+						echo '<th style="width:15%">Dateityp</th>';
+						echo '<th style="width:15%">Dateigröße</th>';
+						echo '<th style="width:15%">Dateipfad</th>';
+						echo '<th style="width:15%">Datum</th>';
+						echo '<th style="width:15%">Beschreibung</th>';
+				    echo '</tr>';
+					
+				    while($row = mysqli_fetch_object($erg)){
+						$size_einheit = "Bytes";
+						$tmp_size = $row->size;
+						if($tmp_size >= 1000) {
+							$size_einheit = "KB";
+							$tmp_size = $tmp_size/1000;
+						}
+						if($tmp_size >= 1000){
+							$size_einheit = "MB";
+							$tmp_size = $tmp_size/1000;
+						}
+						if($tmp_size >= 1000){
+							$size_einheit = "GB";
+							$tmp_size = $tmp_size/1000;
+						}
+						$tmp_size = number_format($tmp_size, 2, ',', '');
+						echo '<tr>';
+							echo '<td style="padding: 5px">'.$row->name.'</td>';
+							echo '<td style="padding: 5px">'.$row->type.'</td>';
+							echo '<td style="padding: 5px">'.$tmp_size.$size_einheit.'</td>';
+							echo '<td style="padding: 5px">'.$row->reference.'</td>';
+							echo '<td style="padding: 5px">'.$row->date.'</td>';
+							echo '<td style="padding: 5px">'.$row->description.'</td>';
+						echo '</tr>';
+					}
+				
+				
+				echo '</table>';
+			}else{
+				echo 'Keine Dateien vorhanden.';
+			}
+			echo '<br><br><br>';
+			echo '<form target="_top" action="eingabeV2.html">';
+			echo '<button type="submit" style="width:15%">Zurück</button>';
+			echo '</form>';
+			echo '</div>';
+		echo '</body>';
+echo '</html>';
 ?>
